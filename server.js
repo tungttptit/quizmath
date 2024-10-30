f6e54f2e5fccfbfd73bbed7790b9d570e70545bd
TUNGTRAN3-ITC-F\tung.tran3 <tung.tran3>
2024-10-31 00:42:41 +0700

    new socket io

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 3000;
+const port = 10000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
